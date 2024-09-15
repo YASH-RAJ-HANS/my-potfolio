cd966b9b3c00cb2c6e77ee491877dac1d8e0b9ec
YASH-RAJ-HANS <yashhans479@gmail.com>
2024-09-15 19:40:52 +0530

    Portfolio intialization

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import BlurIn from "../@/components/magicui/blur-in";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <BlurIn word={"Hello World"}></BlurIn>on the Vite and React logos to learn more
+      
+    </>
+  )
+}
+
+export default App
